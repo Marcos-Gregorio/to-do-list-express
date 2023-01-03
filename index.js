d7dfd2cdc5047bf73794b15282e2ed4de231c837
Marcos-Gregorio <marcosgregs.frontend@gmail.com>
2023-01-03 14:54:26 -0300

    Aula - Entendo mais de expresse e nodemon

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const express = require('express')
+const express = require('express');
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Lista de Tarefas:</h1>')
+})
+
+app.listen(8000,() => {
+    console.log('Servidor Iniciado')
+}) 
