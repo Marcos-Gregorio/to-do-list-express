29f4d722a4d73e8db8ca583c0606392d8edde2ce
Marcos  Gregório <86073733+marcos-gregs@users.noreply.github.com>
2023-05-16 12:21:28 -0300

    Middleware

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express()
 
+// Utilizando middlewares
+
+// Verifica e tem algum json.
+app.use(express.json())
+
+// Middleware
+const Log =(req,res,next) =>{
+    // Next é oq permite que o middleware passe a requisição.
+    console.log(req.body);
+    console.log(`Data:${Date.now()}`);
+    next()
+}
+
+app.use(Log)
+
 app.get('/',(req,res)=>{
     res.send('<h1>Lista de Tarefas:</h1>')
 })
 
 app.get('/json',(req,res)=>{
+   
     res.json({title:'Tarefa X',done: true})
 })
 
