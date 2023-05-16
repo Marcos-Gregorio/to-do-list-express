7adb22f4d6149447fc2541fcda04674e7cfb51c0
Marcos  Gregório <86073733+marcos-gregs@users.noreply.github.com>
2023-05-16 12:03:28 -0300

    Manipulando Json

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/',(req,res)=>{
     res.send('<h1>Lista de Tarefas:</h1>')
 })
 
+app.get('/json',(req,res)=>{
+    res.json({title:'Tarefa X',done: true})
+})
+
 app.listen(8000,() => {
     console.log('Servidor Iniciado')
 }) 
