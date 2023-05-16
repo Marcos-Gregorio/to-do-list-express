86b90421aebd26b25af5d238e648f9fcb045f951
Marcos  Gregório <86073733+marcos-gregs@users.noreply.github.com>
2023-05-16 12:38:19 -0300

    Concentrando Rotas

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
 const express = require('express');
+const checklistRouter = require("./src/routes/checklist")
 const app = express()
 
 // Utilizando middlewares
 
 // Verifica e tem algum json.
 app.use(express.json())
+app.use('/checklist', checklistRouter)
 
-// Middleware
-const Log =(req,res,next) =>{
-    // Next é oq permite que o middleware passe a requisição.
-    console.log(req.body);
-    console.log(`Data:${Date.now()}`);
-    next()
-}
-
-app.use(Log)
-
-app.get('/',(req,res)=>{
-    res.send('<h1>Lista de Tarefas:</h1>')
+app.listen(3000,()=>{
+    console.log('Server on')
 })
-
-app.get('/json',(req,res)=>{
-   
-    res.json({title:'Tarefa X',done: true})
-})
-
-app.listen(8000,() => {
-    console.log('Servidor Iniciado')
-}) 
